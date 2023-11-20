d54a89953cec60ade960de349297734bbf81855d
kangkai <kai.kang@metalpha.finance>
2023-11-20 17:15:29 +0800

    ResponseData 构造

--- lib/model/response_data.dart
+++ lib/model/response_data.dart
@@ -1,6 +1,17 @@
 class ResponseData<T> {
-  int? code;
-  String? message;
-  bool success = false;
-  T? result;
+  final int? code;
+  final String? message;
+  final bool success;
+  final T? result;
+
+  const ResponseData({this.code, this.message, this.success = false, this.result});
+
+  const ResponseData.error(this.code, this.message)
+      : success = false,
+        result = null;
+
+  const ResponseData.success(this.result)
+      : success = true,
+        code = null,
+        message = null;
 }
